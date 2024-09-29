d6a327621a6887786cf9c898f3f679f9a726bc3a
none3284 <the.unknown6390@gmail.com>
2024-09-29 15:34:29 +0800

    teach-main 03:34
    增加殺敵記數表與程式碼

---
+++ Assets/Scripts/objinter.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objinter : MonoBehaviour
+{
+    int score = 0;
+    public Text scoreTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreTxt.text = "殺敵數量=0";
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == "XXXXXX")
+        {
+            collision.gameObject.SetActive(false);
+            score += 1;
+            scoreTxt.text = "殺敵數量="+ score.ToString();
+            Debug.Log("score=" + score.ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
